bc37591f1942f09b77e0a30c34ffcb488d35d9a6
BrianMwendwa180 <brianmwendwa180@gmail.com>
2025-08-20 10:19:58 +0300

    Brian commit

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,19 @@
 import { createContext, useState, useEffect } from 'react';
 import assets from "../assets/assets";
 import { useNavigate } from 'react-router-dom';
-import humanizeDuration from 'humanize-duration';
+import humanizeDuration from 'humanize-duration'
+import { useAuth, useUser } from '@clerk/clerk-react';
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-    const currency = import.meta.env.VITE_CURRENCY;
+    const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
+     
+    const {getToken} = useAuth()
+    const { user } = useUser()
+    
+    
     const [allCourses, setAllCourses] = useState([])
     const [isEducator, setIsEducator] = useState([true])
     const [enrolledCourses, setEnrolledCourses] = useState([])
@@ -76,6 +82,16 @@ const calculateCourseDuration = (course)=>{
         fetchAllCourses()
         fetchUserEnrolledCourses()
     }, []);
+
+    const logToken = async ()=>{
+        console.log(await getToken());
+    }
+
+    useEffect(()=>{
+        if(user){
+            logToken()
+        }
+    },[user])
 
     const value = {
         currency, allCourses, navigate, calculateRating,
